27b1cb8a92fd19ed7490b906d94e7c3c88791396
evopen <520dhh@gmail.com>
2021-07-06 17:13:09 +0800

    2.2 creating an image file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// based on ray tracing in one weekend 3.2.3
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for i in 0..image_height {
+        for j in 0..image_width {
+            let r = i as f64 / (image_height - 1) as f64;
+            let g = j as f64 / (image_width - 1) as f64;
+            let b = 0.25;
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+            print!("{} {} {}\t", ir, ig, ib);
+        }
+        println!();
+    }
 }

ea2ad3afb54f970352468b4f5996cb9197935895
evopen <520dhh@gmail.com>
2021-07-07 10:25:16 +0800

    use png format

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,20 @@ fn main() {
     let image_height = 256;
 
     // Render
-    println!("P3\n{} {}\n255\n", image_width, image_height);
+    let mut image_buffer = image::RgbImage::new(image_width, image_height);
 
-    for i in 0..image_height {
-        for j in 0..image_width {
-            let r = i as f64 / (image_height - 1) as f64;
-            let g = j as f64 / (image_width - 1) as f64;
+    for j in (0..image_height).rev() {
+        println!("\rScanlines remaining: {}", j);
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
             let b = 0.25;
             let ir = (255.99 * r) as u8;
             let ig = (255.99 * g) as u8;
             let ib = (255.99 * b) as u8;
-            print!("{} {} {}\t", ir, ig, ib);
+            image_buffer.put_pixel(i, j, image::Rgb([ir, ig, ib]));
         }
-        println!();
     }
+    println!("Done");
+    image_buffer.save("result.png").unwrap();
 }

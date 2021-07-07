fb942bad5b8355b1632593873b27f37676346e06
evopen <520dhh@gmail.com>
2021-07-07 11:42:21 +0800

    use lower left corner for uv(0.0,0.0) and xy(0,0)

--- src/color.rs
+++ src/color.rs
@@ -6,5 +6,5 @@ pub fn write_color(img: &mut image::RgbImage, x: u32, y: u32, color: &Color) {
     let ir = (255.999 * color.x) as u8;
     let ig = (255.999 * color.y) as u8;
     let ib = (255.999 * color.z) as u8;
-    img.put_pixel(x, y, image::Rgb([ir, ig, ib]));
+    img.put_pixel(x, img.height() - 1 - y, image::Rgb([ir, ig, ib]));
 }

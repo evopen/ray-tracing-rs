90165184cee6059b3ae6bc177a6fbe9c55d473b4
evopen <520dhh@gmail.com>
2021-07-07 12:19:14 +0800

    5.2 red sphere

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,25 @@
 mod color;
 mod ray;
 mod vec3;
+use std::ops::Sub;
+
 use color::Color;
 use ray::Ray;
 use vec3::{Point3, Vec3};
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - *center;
+    let a = r.direction().length_squared();
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().normalize();
     let t = (unit_direction.y + 1.0) * 0.5;
     Color::splat(1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t

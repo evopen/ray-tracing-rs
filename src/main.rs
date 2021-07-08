d33171e785f57bd043dff21dd1799d2e1f28c664
evopen <520dhh@gmail.com>
2021-07-08 16:56:30 +0800

    7.2 antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // based on ray tracing in one weekend 3.2.3
 
+mod camera;
 mod color;
 mod hittable;
 mod hittable_list;
@@ -15,6 +16,7 @@ use hittable::HitRecord;
 use ray::Ray;
 use vec3::{Point3, Vec3};
 
+use crate::camera::Camera;
 use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
 use crate::sphere::Sphere;
@@ -48,6 +50,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
 
     // World
     let mut world = HittableList::new();
@@ -65,20 +68,23 @@ fn main() {
     let lower_left_corner =
         origin - Vec3::new(0.0, 0.0, focal_length) - horizontal / 2.0 - vertical / 2.0;
 
+    // Camera
+    let cam = Camera::default();
+
     // Render
     let mut image_buffer = image::RgbImage::new(image_width, image_height);
 
     for y in (0..image_height).rev() {
         println!("\rScanlines remaining: {}", y);
-        let v = y as f64 / (image_height - 1) as f64;
         for x in 0..image_width {
-            let u = x as f64 / (image_width - 1) as f64;
-            let r = Ray::new(
-                &origin,
-                &(lower_left_corner + u * horizontal + v * vertical - origin),
-            );
-            let pixel_color = ray_color(&r, &world);
-            color::write_color(&mut &mut image_buffer, x, y, &pixel_color);
+            let mut pixel_color = Color::splat(0.0);
+            for _ in 0..samples_per_pixel {
+                let u = (x as f64 + utils::random_f64()) / (image_width - 1) as f64;
+                let v = (y as f64 + utils::random_f64()) / (image_height - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            color::write_color(&mut image_buffer, x, y, &pixel_color, samples_per_pixel);
         }
     }
     println!("Done");

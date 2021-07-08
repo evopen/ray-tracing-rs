d97359985fe7cf89035b02f54a04f6a1326a8e1c
evopen <520dhh@gmail.com>
2021-07-08 18:17:03 +0800

    8.2 rays bounce inside the sphere

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,14 @@ use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
 use crate::sphere::Sphere;
 
-fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
+fn ray_color(r: &Ray, world: &dyn Hittable, depth: u32) -> Color {
+    if depth <= 0 {
+        return Color::splat(0.0);
+    }
     let mut rec = HitRecord::default();
     if world.hit(r, 0.0, std::f64::INFINITY, &mut rec) {
         let target = rec.p + rec.normal + utils::rand_vec3_in_unit_sphere();
-        return 0.5 * ray_color(&Ray::new(&rec.p, &(target - rec.p)), world);
+        return 0.5 * ray_color(&Ray::new(&rec.p, &(target - rec.p)), world, depth - 1);
     }
 
     let unit_direction = r.direction().normalize();
@@ -39,6 +42,7 @@ fn main() {
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
     let samples_per_pixel = 100;
+    let max_depth = 50;
 
     // World
     let mut world = HittableList::new();
@@ -70,7 +74,7 @@ fn main() {
                 let u = (x as f64 + utils::rand_f64()) / (image_width - 1) as f64;
                 let v = (y as f64 + utils::rand_f64()) / (image_height - 1) as f64;
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, &world);
+                pixel_color += ray_color(&r, &world, max_depth);
             }
             color::write_color(&mut image_buffer, x, y, &pixel_color, samples_per_pixel);
         }

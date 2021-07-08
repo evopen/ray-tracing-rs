997df786e07ec7c1f1a848dcbc8751e0e168fda4
evopen <520dhh@gmail.com>
2021-07-08 15:12:20 +0800

    6.4 hit point normal face forward

--- src/hittable.rs
+++ src/hittable.rs
@@ -6,6 +6,18 @@ pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = r.direction().dot(*outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
 }
 
 pub trait Hittable {
--- src/sphere.rs
+++ src/sphere.rs
@@ -40,7 +40,8 @@ impl crate::hittable::Hittable for Sphere {
         }
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - self.center) / self.radius;
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, &outward_normal);
 
         return true;
     }

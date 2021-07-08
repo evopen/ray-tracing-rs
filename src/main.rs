78fc868650af5261bc9d8ea3d123b4ac88bf7dee
evopen <520dhh@gmail.com>
2021-07-08 14:29:15 +0800

    6.1 visualizing normal

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,33 @@
 mod color;
 mod ray;
 mod vec3;
-use std::ops::Sub;
 
 use color::Color;
 use ray::Ray;
 use vec3::{Point3, Vec3};
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> Option<f64> {
     let oc = r.origin() - *center;
     let a = r.direction().length_squared();
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    if let Some(t) = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalize();
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+    } else {
+        let unit_direction = r.direction().normalize();
+        let t = (unit_direction.y + 1.0) * 0.5;
+        Color::splat(1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
     }
-    let unit_direction = r.direction().normalize();
-    let t = (unit_direction.y + 1.0) * 0.5;
-    Color::splat(1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
 fn main() {

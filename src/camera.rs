9b269d9a210f8aa2cbe5c4beb4cf6fddb5bf0d4f
evopen <520dhh@gmail.com>
2021-07-12 16:27:36 +0800

    11.1 adjustable fov

--- src/camera.rs
+++ src/camera.rs
@@ -10,6 +10,32 @@ pub struct Camera {
 }
 
 impl Camera {
+    /// Brief.
+    ///
+    /// Description.
+    ///
+    /// * `vfov` - Vertical field of view in degree
+    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
+        let theta = vfov.to_radians();
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
+        let viewport_width = aspect_ratio * viewport_height;
+
+        let focal_length = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - Vec3::new(0.0, 0.0, focal_length) - horizontal / 2.0 - vertical / 2.0;
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray::new(
             self.origin,
@@ -23,6 +49,7 @@ impl Default for Camera {
         let aspect_ratio = 16.0 / 9.0;
         let viewport_height = 2.0;
         let viewport_width = aspect_ratio * viewport_height;
+
         let focal_length = 1.0;
 
         let origin = Point3::new(0.0, 0.0, 0.0);

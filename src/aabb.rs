5a00f45298443c193f94ffcb1cc51162a430ce92
evopen <520dhh@gmail.com>
2021-07-16 12:39:13 +0800

    3.5 optimize

--- src/aabb.rs
+++ src/aabb.rs
@@ -26,10 +26,12 @@ impl AABB {
         let mut t_min = t_min;
         let mut t_max = t_max;
         for i in 0..3 {
-            let t0 = ((self.minimum[i] - r.origin()[i]) / r.direction()[i])
-                .min((self.maximum[i] - r.origin()[i]) / r.direction()[i]);
-            let t1 = ((self.minimum[i] - r.origin()[i]) / r.direction()[i])
-                .max((self.maximum[i] - r.origin()[i]) / r.direction()[i]);
+            let inv_d = 1.0 / r.direction()[i];
+            let mut t0 = (self.minimum[i] - r.origin()[i]) * inv_d;
+            let mut t1 = (self.maximum[i] - r.origin()[i]) * inv_d;
+            if inv_d < 0.0 {
+                std::mem::swap(&mut t0, &mut t1);
+            }
             t_min = t0.max(t_min);
             t_max = t1.min(t_max);
             if t_max <= t_min {

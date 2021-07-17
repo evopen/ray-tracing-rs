f3f45fc25f76095daef348b4fe6461b2e6df64b9
evopen <520dhh@gmail.com>
2021-07-17 19:13:47 +0800

    5.2 smooth the result

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,3 +1,5 @@
+use std::ops::BitAnd;
+
 use crate::utils;
 use crate::vec3::Point3;
 
@@ -48,13 +50,47 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        let max = POINT_COUNT - 1;
-        let i = ((4.0 * p.x) as i32 & max as i32) as usize;
-        let j = ((4.0 * p.y) as i32 & max as i32) as usize;
-        let k = ((4.0 * p.z) as i32 & max as i32) as usize;
+        let max = (POINT_COUNT - 1) as i32;
 
-        let idx = self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k];
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
 
-        return self.ranfloat[idx];
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+
+        let mut color: [[[f64; 2]; 2]; 2] = Default::default();
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    color[di][dj][dk] = self.ranfloat[self.perm_x
+                        [((i + di as i32).bitand(max)) as usize]
+                        ^ self.perm_y[((j + dj as i32).bitand(max)) as usize]
+                        ^ self.perm_z[((k + dk as i32).bitand(max)) as usize]]
+                }
+            }
+        }
+
+        return Self::trilinear_interp(color, u, v, w);
+    }
+
+    fn trilinear_interp(color: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accumulate = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let fi = i as f64;
+                    let fj = j as f64;
+                    let fk = k as f64;
+                    let weight = (fi * u + (1.0 - fi) * (1.0 - u))
+                        * (fj * v + (1.0 - fj) * (1.0 - v))
+                        * (fk * w + (1.0 - fk) * (1.0 - w));
+                    accumulate += color[i][j][k] * weight;
+                }
+            }
+        }
+        accumulate
     }
 }

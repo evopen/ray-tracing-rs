3f1d77ebd47c35bc2d390e03eaec7392a106a064
evopen <520dhh@gmail.com>
2021-07-17 19:28:19 +0800

    5.3 Hermite cubic?

--- src/perlin.rs
+++ src/perlin.rs
@@ -52,9 +52,12 @@ impl Perlin {
     pub fn noise(&self, p: Point3) -> f64 {
         let max = (POINT_COUNT - 1) as i32;
 
-        let u = p.x - p.x.floor();
-        let v = p.y - p.y.floor();
-        let w = p.z - p.z.floor();
+        let mut u = p.x - p.x.floor();
+        let mut v = p.y - p.y.floor();
+        let mut w = p.z - p.z.floor();
+        u = u * u * (3.0 - 2.0 * u);
+        v = v * v * (3.0 - 2.0 * v);
+        w = w * w * (3.0 - 2.0 * w);
 
         let i = p.x.floor() as i32;
         let j = p.y.floor() as i32;

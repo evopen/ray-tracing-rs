267c32a94ad447799ff6721ae95644f17048d206
evopen <520dhh@gmail.com>
2021-07-18 20:31:21 +0800

    fix zero vector check

--- src/vec3.rs
+++ src/vec3.rs
@@ -2,7 +2,8 @@ pub use glam::DVec3 as Vec3;
 
 pub type Point3 = Vec3;
 
+#[inline]
 pub fn is_near_zero(v: Vec3) -> bool {
     let epsilon = 1e-8;
-    v.cmple(Vec3::splat(epsilon)).all()
+    v.abs().cmple(Vec3::splat(epsilon)).all()
 }
